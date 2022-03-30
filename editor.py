8d6421813666ef20b8fe96117ac3d538d249d7b2
iferdel <ignaferdel@gmail.com>
2022-03-30 19:55:54 -0300

    update ffmpeg_converter

--- editor.py
+++ editor.py
@@ -30,11 +30,12 @@ class VideoCropper(VideoEditor):
 
     def __init__(self, pattern, input_folder, output_folder):
         super(VideoCropper, self).__init__(pattern, input_folder, output_folder)    
-        
+
         self.resolution = [(subprocess.check_output(f'ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 {os.path.join(self.files_input_path, file)}',
                             shell=True)).decode('utf-8').strip() for file in self.files]
     
     def cropper_dataframe(self):
+        # TODO: cropper_boundaries as input
         df = pd.DataFrame(data={'file': self.files, 'resolution': self.resolution})
         df['resolution'] = df.loc[:, 'resolution'].str.findall('(\d{4}x\d{4})').str[0]
         df['file'] = df.loc[:,'file'].str.split(".")
@@ -48,8 +49,6 @@ class VideoCropper(VideoEditor):
                                                         'ffmpeg -i {} -b:v 5M {}'.format(os.path.join(self.files_input_path, df.loc[item,'file']),
                                                         os.path.join(self.files_output_path, df.loc[item,'crop_output_file'])))
         return df
-    
-#   TODO def cropper_boundaries(self) TODO
 
 
 class VideoTrimmer(VideoEditor):
@@ -65,11 +64,29 @@ class VideoTrimmer(VideoEditor):
             with open(os.path.join(self.path, "files_to_txt.txt"), "w") as outfile: outfile.write("\n".join(txt)) 
         else: input("Make sure to define de trim margins. Press enter to continue...")
    
-    def trim_margins_from_file(self):
+    def trimmer_dataframe(self):
         with open(os.path.join(self.files_input_path, "files_to_txt.txt")) as f:
             next(f)
             txt = [i.split('\n')[0].strip() for i in [line for line in f]]
-            txt = [item.split(' ') for item in txt]
-            files = [item[0] for item in txt]
-            trim_parameter = [list(filter(None, item[1:])) for item in txt]
-            return files, trim_parameter
+        txt = [item.split(' ') for item in txt]
+        files = [item[0] for item in txt]
+        trim_parameter = [list(filter(None, item[1:])) for item in txt]
+        df = pd.DataFrame(data={'file': files, 'ti-tf': trim_parameter})
+        del files, trim_parameter
+        df = df.apply(pd.Series.explode)
+        df['tf'] = df['ti-tf'].shift(-1) #shift(-1) to generate the pattern ti-tf
+        df.rename(columns={'ti-tf': 'ti'}, inplace=True)
+        df = df.iloc[:-1] # remove last row
+        df = df[0::2] # take back only the rows with pair indexes from the explode (to avoid having tf-ti pattern)
+        df['ti'] = df['ti'].str.strip('[]'); df['tf'] = df['tf'].str.strip('[]') #strip brackets from trim range as they are not included in ffmpeg syntax 
+        df['file'] = df.loc[:,'file'].str.split(".")
+        df['trim_output_file'] = df.loc[:, 'file'].str[0] + '_ti_' + df.loc[:, 'ti'] + '_tf_' + df.loc[:, 'tf'] + \
+                                        '.' + df.loc[:, 'file'].iloc[0][1]
+        df['trim_output_file'] = [x.replace(':', '_') for x  in df.loc[:, 'trim_output_file'].to_numpy()]
+        df['file'] = df.loc[:,'file'].str[0] + '.'+ df.loc[:,'file'].iloc[0][1]
+        df = df.reset_index(drop=True)
+        for item in df.index:
+            df.loc[item, 'ffmpeg_trim_call'] = 'ffmpeg -ss {} -i {} -to {} -minrate 10M -c:v copy {}'.format(
+                                    df.loc[item,'ti'], os.path.join(self.files_input_path, df.loc[item,'file']), df.loc[item,'tf'], 
+                                    os.path.join(self.files_output_path, df.loc[item,'trim_output_file']))   
+        return df                            
--- ffmpeg_converter.py
+++ ffmpeg_converter.py
@@ -1,51 +1,32 @@
 from editor import VideoCropper, VideoTrimmer
 from effects import VideoEffects
-import pandas as pd
-import os
 import multiprocessing
-
-def trim_dataframe(file_list, trim_list):
-    df = pd.DataFrame(data={'file': file_list, 'ti-tf': trim_list})
-    df = df.apply(pd.Series.explode)
-    df['tf'] = df['ti-tf'].shift(-1) #shift(-1) to generate the pattern ti-tf
-    df.rename(columns={'ti-tf': 'ti'}, inplace=True)
-    df = df.iloc[:-1] # remove last row
-    df = df[0::2] # take back only the rows with pair indexes from the explode (to avoid having tf-ti pattern)
-    df['ti'] = df['ti'].str.strip('[]'); df['tf'] = df['tf'].str.strip('[]') #strip brackets from trim range as they are not included in ffmpeg syntax 
-    return df
 
 def trimmer():
     trim = VideoTrimmer(pattern='.*((?=.mp4|.MP4|.mov|.MOV))', input_folder='InputToTrim', output_folder='OutputFromTrim') 
-    df = trim_dataframe(file_list=trim.trim_margins_from_file()[0], trim_list=trim.trim_margins_from_file()[1])
-    df['file'] = df.loc[:,'file'].str.split(".")
-    df['trim_output_file'] = df.loc[:, 'file'].str[0] + '_ti_' + df.loc[:, 'ti'] + '_tf_' + df.loc[:, 'tf'] + \
-                                       '.' + df.loc[:, 'file'].iloc[0][1]
-    df['trim_output_file'] = [x.replace(':', '_') for x  in df.loc[:, 'trim_output_file'].to_numpy()]
-    df['file'] = df.loc[:,'file'].str[0]+'.'+df.loc[:,'file'].iloc[0][1]
-    df = df.reset_index(drop=True)
-    for item in df.index:
-        df.loc[item, 'ffmpeg_trim_call'] = 'ffmpeg -ss {} -i {} -to {} -minrate 10M -c:v copy {}'.format(
-                                  df.loc[item,'ti'], os.path.join(trim.files_input_path, df.loc[item,'file']), df.loc[item,'tf'], 
-                                  os.path.join(trim.files_output_path, df.loc[item,'trim_output_file']))
+    df = trim.trimmer_dataframe()
     p = multiprocessing.Process(target=trim.ffmpeg_call, args=(df, 'ffmpeg_trim_call'))
     p.start()
+    p.join()
 
 def cropper():
     crop = VideoCropper(pattern='.*((?=.mp4|.MP4|.mov|.MOV))', input_folder='OutputFromTrim', output_folder='OutputFromCrop')
     df = crop.cropper_dataframe()
     p = multiprocessing.Process(target=crop.ffmpeg_call, args=(df, 'ffmpeg_crop_call'))
     p.start()
+    p.join()
 
 def speed_effects(speed_rate):
     effects = VideoEffects(pattern='.*((?=.mp4|.MP4|.mov|.MOV))', input_folder='OutputFromCrop', output_folder='OutputFromEffects')
     df = effects.speed_rate_dataframe(speed_rate)
     p = multiprocessing.Process(target=effects.ffmpeg_call, args=(df, 'ffmpeg_effects_call'))
     p.start()
+    p.join()
 
 def main():
     trimmer()
     cropper()
-    speed_effects(0.4)
-
+    speed_effects(0.8)
+    
 if __name__=='__main__':
     main()
